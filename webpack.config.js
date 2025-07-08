db719eb4436230bf3377ce0527fc7176f6b732e0
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-07-09 00:50:35 +0400

    fix some webpack updates needed

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
+  experiments: {
+    asyncWebAssembly: true,
+  },
   entry: "./bootstrap.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
   mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+  plugins: [new CopyWebpackPlugin(["index.html"])],
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/,
+        type: "webassembly/async", // Or "webassembly/sync" if you chose syncWebAssembly above
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".wasm", ".mjs", ".js", ".json"],
+  },
 };
